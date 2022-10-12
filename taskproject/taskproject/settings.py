5363baff449aa8110d63db9fde31599c6c0fda77
Furkan Ar <63981707+FurkanAr@users.noreply.github.com>
2022-10-12 19:25:36 +0300

    Update settings.py

--- taskproject/taskproject/settings.py
+++ taskproject/taskproject/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "TaskDB",
         'USER' : 'postgres',
-        'PASSWORD' : 12345,
+        'PASSWORD' : "",
         'HOST' : 'localhost'
     }
 }
